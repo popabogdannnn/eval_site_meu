261d0cd6fc8e69aa72c13db63e593352ca46c69b
popa.bogdannnn <popa_bogdanioan@yahoo.com>
2021-11-23 18:17:50 +0200

    Only using one jail now

--- restore.sh
+++ restore.sh
@@ -6,14 +6,12 @@ sudo chown -R bogdan:bogdan /sys/fs/cgroup/cpuacct/ia-sandbox
 sudo chown -R bogdan:bogdan /sys/fs/cgroup/memory/ia-sandbox
 sudo chown -R bogdan:bogdan /sys/fs/cgroup/pids/ia-sandbox
 
-mkdir eval/execution_jail/
-sudo mount -t tmpfs -o size=512m swap eval/execution_jail
-mkdir eval/compilation_jail/
-sudo mount -t tmpfs -o size=512m swap eval/compilation_jail
+mkdir eval/jail/
+sudo mount -t tmpfs -o size=1024m swap eval/jail
 
 echo "0" | sudo tee /sys/kernel/mm/transparent_hugepage/khugepaged/defrag
 echo "madvise" | sudo tee /sys/kernel/mm/transparent_hugepage/defrag
 echo "madvise" | sudo tee /sys/kernel/mm/transparent_hugepage/enabled
 
-sysctl kernel.randomize_va_space=0
+sudo sysctl kernel.randomize_va_space=0
 sudo swapoff -a
